bdcd0d24eb28c638921824c935c49aebc30b1443
Rickelle <rickelle.1993@live.com.au>
2022-12-08 19:53:12 +1030

    age-page-drafts/bar-seeds

---
+++ Develop/seeds/bar-seeds.js
@@ -0,0 +1,23 @@
+const { User } = require('../models')
+
+const userData = [
+{
+    name: "1000 Island",
+    address: "15 Solomon St, Adelaide SA 5000",
+    rating: 2
+},
+{
+    name: "Hains & Co",
+    address: "23 Gilbert Pl, Adelaide SA 5000",
+    rating: 4
+},
+{
+    name: "NOLA",
+    address: "28 Vardon Ave, Adelaide SA 5000",
+    rating: 5
+}
+]
+
+const seedUsers =() => User.bulkCreate(userData);
+
+module.exports = seedUsers; 
