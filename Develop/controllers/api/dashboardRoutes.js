f85f84e02e4c087ff68dbd2357c35411a14fbabd
lilianaba <52921112+lilianaba@users.noreply.github.com>
2022-12-11 20:30:30 +1030

    get the bar rating from posts

--- Develop/controllers/api/dashboardRoutes.js
+++ Develop/controllers/api/dashboardRoutes.js
@@ -1,4 +1,39 @@
 // select avg(rating) rating
 //  from post 
-//  where bar_id = bar_id 
+//  where bar_id = selected_bar_id 
 //  group by bar_id
+
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//connect to database
+const bd = mysql.createConnection(
+    {
+        host:'localhost',
+        user: 'root',
+        password: '',
+        database: "tipsy_db"
+    },
+    console.log(`connected to tipsy_db`);
+);
+
+//get bar rating
+app.get('/api/bar/:id',(req,res)=>{
+    const sql = `select avg(rating) rating from post where bar_id = ? group by bar_id`;
+    db.query(sql,(err,rows)=>{
+        if(err) {
+            res.status(500).json({error:err.message});
+            return;
+        }
+        res.json({
+            message:'success',
+            data:rows
+        });
+    });
+});
