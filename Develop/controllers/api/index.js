c7ad73c4cec0be4cd24b926731610c0b0c2896f2
Rickelle <rickelle.1993@live.com.au>
2022-12-12 19:20:33 +1030

    Post-routes/api-index modifications

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,9 +1,13 @@
-// const router = require('express').Router();
-// const userRoutes = require('./userRoutes');
-// const projectRoutes = require('./projectRoutes');
-
-
-// router.use('/users', userRoutes);
-// router.use('/projects', projectRoutes);
-
-// module.exports = router;
+// This is the setup for the '/api/' routes
+const router = require('express').Router();
+// This will grab and set up the route files as variables
+const userRoutes = require('./user-routes');
+const barRoutes = require('./bar-routes');
+// NEED TO-DO CREATE FILE postRoutes
+const postRoutes = require('./post-routes');
+// we then use them here to set them up to Express router we will have one set up for every api routes
+router.use('/users', userRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/posts', postRoutes);
+// exports
+module.exports = router;
