927876793bef5807ee1909185c5a22231fc7cc60
Rickelle <rickelle.1993@live.com.au>
2022-12-12 18:50:43 +1030

    update

---
+++ Develop/controllers/homeRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+
+      res.render('./public/js/age.js')});
--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,7 +1,13 @@
-// const router = require('express').Router();
-// const apiRoutes = require('./api');
-// const homeRoutes = require('./homeRoutes');
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 
-// router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
-// module.exports = router;
+
+module.exports = router;
+
+
+
+
