092c531d956eb4a7a4168c25f90147facb57aed7
Rickelle <rickelle.1993@live.com.au>
2022-12-13 18:04:25 +1030

    comment-route-POST

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,19 +4,12 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const barRoutes = require('./bar-routes');
 const postRoutes = require('./post-routes');
-<<<<<<< HEAD
 const commentRoutes = require('./comment-routes');
-
-router.use('/users', userRoutes);
-router.use('/bars', barRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
-=======
 // we then use them here to set them up to Express router we will have one set up for every api routes
 // router.use('/users', userRoutes);
 // router.use('/dashboard', dashboardRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 // router.use('/bar', barRoutes);
->>>>>>> 20b0f44d3ec312c091909ce5bd0742ca5ee63f99
 // exports
 module.exports = router;
