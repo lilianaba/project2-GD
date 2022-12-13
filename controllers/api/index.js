027940bbce2408d8acc781eae29945122ed0c62f
Lara Grocke <laragrocke@Laras-MacBook-Air.local>
2022-12-13 20:15:15 +1030

    Editing routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,17 +1,13 @@
-// This is the setup for the '/api/' routes
 const router = require('express').Router();
 
-// This will grab and set up the route files as variables
 const userRoutes = require('./user-routes');
-const barRoutes = require('./bar-routes');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+// const barRoutes = require('./bar-routes');
+// const postRoutes = require('./post-routes');
+// const commentRoutes = require('./comment-routes');
 
-// we then use them here to set them up to Express router we will have one set up for every api routes
 router.use('/users', userRoutes);
-router.use('/bar', barRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+// router.use('/bar', barRoutes);
+// router.use('/posts', postRoutes);
+// router.use('/comments', commentRoutes);
 
-// export
 module.exports = router;
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ 
   //helpers 
+
 });
 
 const sess = {
@@ -32,7 +33,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(session(sess));
+app.use(session(sess));
 app.use(routes);
 
 sequelize.sync({ force: true }).then(() => {
