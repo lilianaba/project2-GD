5976f8d5eac39114a74cc949835adff9875fb63d
Rickelle <rickelle.1993@live.com.au>
2022-12-15 21:53:37 +1030

    createpostJS/handlebar

---
+++ public/js/addpost.js
@@ -0,0 +1,25 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="title"]').value;
+    const post_content = document.querySelector('textarea[name="post_content"]').value.trim();
+// need to add in the rating logic --- 
+
+
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            post_content,
+            rating
+        }),
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create post');
+      }
+};
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
