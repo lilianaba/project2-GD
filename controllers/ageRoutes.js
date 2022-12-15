fc0dfc814d41067c0914e0b0d1be934b42896610
lilianaba <52921112+lilianaba@users.noreply.github.com>
2022-12-15 18:34:24 +1030

    adding few thing

---
+++ controllers/ageRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+// const withAuth = require('../utils/auth');
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
