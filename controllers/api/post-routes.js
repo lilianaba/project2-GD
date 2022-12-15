3ff3c8fa4da27269215b9500779ae32ed3ea0188
Rickelle <rickelle.1993@live.com.au>
2022-12-15 18:15:14 +1030

    post routing

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,79 +1,83 @@
-// // gets the models
-// const { User, Post, Comment } = require('../../models');
-// const { Bar } = require('../../models/Bar');
-// // sets up the Express Router
-// const router = require('express').Router();
-// // this will connect in the helpers utils - for them to only be able to acces once logged in
-// // const withAuth = require('../../utils/auth');
-// //=======================================================================
-// // To create a new post(review) the user will use the method POST
-// router.get('/', async (req, res) => {
-//   try {
-//     const barData = await Bar.findAll();
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-//     const users = barData.map((project) => project.get({ plain: true }));
 
-//     res.json(users);
+// gets all review posts
+router.get("/", async (req, res) => {
+    try {
+        const postData = await Post.findAll({})
+        res.status(200).json(postData)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
+});
 
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+// Makes a review 
+router.post("/", withAuth, async (req, res) => {
+    try {
+        console.log("creating");
+        const postData = await Post.create({
+            title: req.body.title,
+            post_content: req.body.post_content,
+            rating: req.session.rating
+        },
+            res.status(200).json(postData))
 
-// router.post('/', async (req, res) => {
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
-//   try {
+// // Update a post -- DONT USE
+// router.put("/:id", withAuth, async (req, res) => {
+// try {
 
-//     const barData = await Bar.findAll({ where: { name: req.body.name } });
-//     console.log(req.body + "Review has been triggered from the user to create a new Review in postRoutes");
+//  const postData = await Post.update({
+//             title: req.body.title,
+//             content: req.body.post_content,
+//         }, {
+//             where: {
+//                 id: req.params.id,
+//             },
+//         })
 
-//     if (barData) {
+//             if (!postData) {
+//                 res.status(404).json({
+//                     message: "No post found with this id"
+//                 });
+//                 return;
+//             }
+//             res.json(postData);
 
-//       const postData = await Post.create({
-//         title: req.body.title,
-//         post_content: req.body.post_content,
-//         user_id: req.session.user_id,
-//         bar_id: req.body.name,
-//         rating: req.body.rating,
-//       });
+//          } catch (err) {
+//             console.log(err);
+//             res.status(500).json(err);
+//         }
+//     });
 
-//       // below will redirect you to the dashboard after the post has been created
-//       res.redirect('/api/dashboard')
 
-//     }
 
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   };
+router.delete("/:id", withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+            },
+        })
+        if (!postData) {
+            res.status(404).json({ message: "No post with that ID found with this id" });
+            return;
+        }
+        res.json(postData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
-// });
 
-// //=======================================================================
-// // To delete a post(review) the user will use the method DELETE and use the params.id to select
-// router.delete('/:id', async (req, res) => {
-//   console.log("DELETING A REVIEW")
-//   try {
-//     const postData = await Post.destroy({
-//       where: req.params.id,
-//     },
-//     )
 
-//     if (!postData) {
-//       console.log("There is no review with this ID");
-//       // Respond with 404 if the ID requests by the user doesnt enter a valid ID
-//       // The HTTP 404 Not Found response status code indicates that the server cannot find the requested resource
-//       res.status(404).json({ message: 'No review found with this ID' });
-//       return;
-//     }
-//     //The HTTP 200 OK success status response code indicates that the request has succeeded.
-//     res.status(200).json(postData)
-//     res.redirect('/api/dashboard')
-//   } catch (err) {
-//     console.log(err);
-//     // The HTTP status code 500 is a generic error response.
-//     res.status(500).json(err);
-//   }
-// })
-
-// module.exports = router;
+module.exports = router;
