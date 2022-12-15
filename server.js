8c1a397011e64e9ce865e659e54346d4953ba017
Rickelle <rickelle.1993@live.com.au>
2022-12-15 20:13:46 +1030

    dashboard fix/helper added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const path = require('path');
-
+const helpers =require('./utils/helper')
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -11,8 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ 
-  //helpers 
-
+  helpers 
 });
 
 const sess = {
