8295d2f817bd46e19be5894d67a15de61b287231
lilianaba <52921112+lilianaba@users.noreply.github.com>
2022-12-18 13:40:09 +1030

    comment tag fixed

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -97,15 +97,15 @@ router.get('/bar/:id', withAuth, async (req, res) => {
 
 router.post('/bar/:id',withAuth, async (req, res) => {
   try {
-    const newpost = await Post.create({
+    const newPost = await Post.create({
 
       title: req.body.post_title,
       content: req.body.description,
       rating: req.body.rating,
       
     });
-    console.log(newpost);
-    res.status(200).json(newpost);
+    console.log(newPost);
+    res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
