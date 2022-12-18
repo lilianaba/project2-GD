6d029a43f631a6f1007e2ed84417681914fd0582
lilianaba <52921112+lilianaba@users.noreply.github.com>
2022-12-18 18:57:45 +1030

    adding some style and bar pictures

--- seeds/bar-seeds.js
+++ seeds/bar-seeds.js
@@ -4,14 +4,17 @@ const barData = [
 {
     name: "1000 Island",
     address: "15 Solomon St, Adelaide SA 5000",
+    img_url: "/media/bar1.jpeg"
 },
 {
     name: "Hains & Co",
     address: "23 Gilbert Pl, Adelaide SA 5000",
+    img_url: "/media/bar2.jpg"
 },
 {
     name: "NOLA",
     address: "28 Vardon Ave, Adelaide SA 5000",
+    img_url: "/media/bar3.jpg"
 }
 ]
 
