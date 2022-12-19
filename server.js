84d2602f755b523b77ce3ed97fdda39f0003b48a
Rickelle <rickelle.1993@live.com.au>
2022-12-19 21:37:02 +1030

    heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const path = require('path');
 // const helpers =require('./utils/helper')
-const sequelize = require('./config/connection');
+const sequelize = require('./config/connection.js');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -23,7 +23,7 @@ const sess = {
     db: sequelize
   })
 };
-
+app.use(session(sess));
 
 
 app.engine('handlebars', hbs.engine);
@@ -32,7 +32,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sess));
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
