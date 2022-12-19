f23a3b28ed830442e3c782b69decd76affd7274b
Rickelle <rickelle.1993@live.com.au>
2022-12-19 22:19:43 +1030

    Updates

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 // const helpers =require('./utils/helper')
 const sequelize = require('./config/connection.js');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,16 +14,7 @@ const hbs = exphbs.create({
   // helpers 
 });
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-app.use(session(sess));
+app.use(session({ secret: "secret", resave: true, saveUninitialized: true }));
 
 
 app.engine('handlebars', hbs.engine);
